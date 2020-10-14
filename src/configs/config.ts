fcac1581d0019014e8ec61fe6bffc2ac8ccf9558
fukkun-pnr <fukushima.takanori@panair.jp>
2020-10-15 06:40:48 +0900

    remove api key

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,4 +1,4 @@
 export const unsplash = {
     domain: "https://api.unsplash.com/",
-    apiKey: "FvPWrJKqhuR0yycM1YFs5zgY9T5NnSOP5eR_4GYx4iY"
+    apiKey: process.env.REACT_APP_UNSPLASH_API_KEY,
 };
