3c85a977bb964811ea809a41a3d6e881964cbff2
fukkun-pnr <fukushima.takanori@panair.jp>
2020-10-15 11:16:13 +0900

    fix test

--- src/components/Indicator.spec.tsx
+++ src/components/Indicator.spec.tsx
@@ -5,13 +5,14 @@ import { Indicator } from "./Indicator";
 describe("components/Indicator", () => {
     it("check props", () => {
         const wrapper = render(<Indicator size={100} />);
-        const indicator = wrapper.getByTestId("indicator");
+        const indicator = wrapper.getAllByRole("generic")[1];
         expect(indicator).not.toBeNull();
         expect(indicator.getAttribute("style")).toBe("width: 100px; height: 100px;");
     });
 
     it("change size", () => {
         const wrapper = render(<Indicator size={200} />);
-        expect(wrapper.getByTestId("indicator").getAttribute("style")).toBe("width: 200px; height: 200px;");
+        const indicator = wrapper.getAllByRole("generic")[1];
+        expect(indicator.getAttribute("style")).toBe("width: 200px; height: 200px;");
     });
 });
